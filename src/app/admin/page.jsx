74c41e39ec023b5372d12c15c0d5d75caaf555c6
Vishal Gautam <ivishalgautamm@gmail.com>
2024-12-18 12:28:44 +0530

    bug fixes

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,11 +1,11 @@
-import AdminLoginForm from "@/components/forms/admin-login";
+import LoginForm from "@/components/forms/login";
 import AuthLayout from "@/components/layout/auth-layout";
 import React from "react";
 
 export default function Page() {
   return (
     <AuthLayout>
-      <AdminLoginForm />
+      <LoginForm />
     </AuthLayout>
   );
 }
