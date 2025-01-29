215f1694e88b68a9bf2f4df3d1b6bea5e5c86435
Vishal Gautam <ivishalgautamm@gmail.com>
2025-01-29 16:56:39 +0530

    removed thank you page

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 const config = {
-  api_base: process.env.NEXT_PUBLIC_API_URL,
+  api_base: process.env.NEXT_PUBLIC_DRDIPTI_API_URL,
   file_base: process.env.NEXT_PUBLIC_IMAGE_DOMAIN,
-  website_base: process.env.NEXT_PUBLIC_WEBSITE_BASE,
+  website_base: process.env.NEXT_PUBLIC_DRDIPTI_IMAGE_DOMAIN,
 };
 
 export default config;
