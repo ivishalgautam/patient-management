a3311484f884fc08e2bec051e801c38a8faa56fa
Vishal Gautam <ivishalgautamm@gmail.com>
2025-04-18 13:45:26 +0530

    removed services pricing

--- src/validation-schemas/service.js
+++ src/validation-schemas/service.js
@@ -16,22 +16,26 @@ export const serviceSchema = z.object({
     .number({ required_error: "Actual price is required." })
     .min(1, { message: "Actual price is required." }),
   discounted_price: z.number().optional(),
-  main_points: z.array(z.string()).optional(),
-  custom_points: z
-    .array(
-      z.object({
-        heading: z
-          .string({
-            required_error: "Heading is required in custom points.",
-          })
-          .min(1, { message: "Heading is required in custom points." }),
-        body: z
-          .array(z.string())
-          .min(2, { message: "Body must have at least 2 items." }),
-      }),
-      {
-        required_error: "Custom points are required.",
-      },
-    )
-    .optional(),
+  // main_points: z
+  //   .array(z.string(), {
+  //     required_error: "Main points are required.",
+  //   })
+  //   .optional(2, { message: "Main points must have at least 2 items." }),
+  // custom_points: z
+  //   .array(
+  //     z.object({
+  //       heading: z
+  //         .string({
+  //           required_error: "Heading is required in custom points.",
+  //         })
+  //         .min(1, { message: "Heading is required in custom points." }),
+  //       body: z
+  //         .array(z.string())
+  //         .min(2, { message: "Body must have at least 2 items." }),
+  //     }),
+  //     {
+  //       required_error: "Custom points are required.",
+  //     },
+  //   )
+  //   .min(1, { message: "Custom points must have at least 2 items." }),
 });
